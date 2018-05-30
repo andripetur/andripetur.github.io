9559521c807b44a594549bc4b9eaf5abceadf162
Andri Pétur <andripeturth@gmail.com>
2018-05-30 11:21:55 +0000

    major revision landing page, papers added, started icelandic portfolio

--- js/freelancer.js
+++ js/freelancer.js
@@ -35,3 +35,14 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+var loadFooter = function(path){
+  var xhr= new XMLHttpRequest();
+  xhr.open('GET', path, true);
+  xhr.onreadystatechange= function() {
+      if (this.readyState!==4) return;
+      if (this.status!==200) return; // or whatever error handling you want
+      document.getElementById('foot').innerHTML= this.responseText;
+  };
+  xhr.send();
+}
